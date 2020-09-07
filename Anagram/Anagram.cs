80e93e91c9a525067d98c104318f150647182761
nayanamangalpalli <41451576+nayanamangalpalli@users.noreply.github.com>
2020-09-07 17:37:49 +0530

    divides method

--- Anagram/Anagram.cs
+++ Anagram/Anagram.cs
@@ -10,27 +10,42 @@ namespace Anagram
             if(word1.Length != word2.Length)
             {
                 return false;
-	        }
+	     }
             else{
-	
-                Dictionary<char,int> TempDictionary = new Dictionary<char,int>();
-                foreach(char charKeyElement in word1) 
-                {
-                    if(TempDictionary.ContainsKey(charKeyElement))
-                        TempDictionary[charKeyElement]++;
-                    else
-                        TempDictionary.Add(charKeyElement, 1);
-                }
-                foreach(char charKeyElement in word2)    
-                {
-                    if(!TempDictionary.ContainsKey(charKeyElement)) 
-                        return false;
-                    TempDictionary[charKeyElement]--;
-                    if(TempDictionary[charKeyElement]== 0) 
-                        TempDictionary.Remove(element);
-                }
-                return TempDictionary.Count == 0;
+			
+                Dictionary<char,int> ReturnedCharKeyIntValueDictionary = addStringCharactersToDictionary(word1);
+		return checkParameterStringCharactersContainInDictionary(ReturnedCharKeyIntValueDictionary,word2);
+	    }
+                
         }
+	
+	public Dictionary<char,int> addStringCharactersToDictionary(string _WordParameter){
+		
+		Dictionary<char,int> _LocalCharKeyIntValueDictionary = new Dictionary<char,int>();
+                
+		foreach(char charKeyElement in _WordParameter) 
+                {
+                    if(_LocalCharKeyIntValueDictionary.ContainsKey(charKeyElement))
+                        _LocalCharKeyIntValueDictionary[charKeyElement]++;
+                    else
+                        _LocalCharKeyIntValueDictionary.Add(charKeyElement, 1);
+                }
+		
+		return _LocalCharKeyIntValueDictionary;
+	}
+		
+	public bool checkParameterStringCharactersContainInDictionary(Dictionary<char,int> _CharKeyIntValueDictionaryParameter, string _WordParameter){
+		
+		foreach(char charKeyElement in _WordParameter)    
+                {
+                    if(!_CharKeyIntValueDictionaryParameter.ContainsKey(charKeyElement)) 
+                        return false;
+                    _CharKeyIntValueDictionaryParameter[charKeyElement]--;
+                    if(_CharKeyIntValueDictionaryParameter[charKeyElement]== 0) 
+			_CharKeyIntValueDictionaryParameter.Remove(charKeyElement);
+                }
+                return _CharKeyIntValueDictionaryParameter.Count == 0;
+	}
             
         public List<string> SelectAnagrams(string word, List<string> candidates) {
             //Insert the correct implementation here
