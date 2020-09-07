de08e25ab4c32859c7e7bbf6a73a8db0aac8e1d4
nayanamangalpalli <41451576+nayanamangalpalli@users.noreply.github.com>
2020-09-07 17:20:40 +0530

    Updates WordPairIsAnagram Method

--- Anagram/Anagram.cs
+++ Anagram/Anagram.cs
@@ -6,9 +6,32 @@ namespace Anagram
     public class AnagramSelector
     {
         public bool WordPairIsAnagram(string word1, string word2) {
-            //Insert the correct implementation here
-            return false;
+            
+            if(word1.Length != word2.Length)
+            {
+                return false;
+	        }
+            else{
+	
+                Dictionary<char,int> TempDictionary = new Dictionary<char,int>();
+                foreach(char charKeyElement in word1) 
+                {
+                    if(TempDictionary.ContainsKey(charKeyElement))
+                        TempDictionary[charKeyElement]++;
+                    else
+                        TempDictionary.Add(charKeyElement, 1);
+                }
+                foreach(char charKeyElement in word2)    
+                {
+                    if(!TempDictionary.ContainsKey(charKeyElement)) 
+                        return false;
+                    TempDictionary[charKeyElement]--;
+                    if(TempDictionary[charKeyElement]== 0) 
+                        TempDictionary.Remove(element);
+                }
+                return TempDictionary.Count == 0;
         }
+            
         public List<string> SelectAnagrams(string word, List<string> candidates) {
             //Insert the correct implementation here
             return candidates;
